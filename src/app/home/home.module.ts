148c6cbbd78591078935caa334435fa110745de3
Phil Schilter <phil.schilter@gmail.com>
2017-04-25 20:49:50 +0200

    :fire: remove unused bank account validator

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,14 +5,12 @@ import {SharedModule} from '../shared/shared.module';
 import {HomeComponent, NewPaymentComponent, LatestTransactionsComponent } from './components/';
 import { AuthModule } from '../auth/auth.module';
 import { HomeRoutingModule } from './home-routing.module';
-import { BankAccountValidatorDirective } from './directives/bankaccount-validator.directive';
 
 @NgModule({
   declarations: [
     HomeComponent,
     NewPaymentComponent,
-    LatestTransactionsComponent,
-    BankAccountValidatorDirective
+    LatestTransactionsComponent
   ],
   imports: [
     HomeRoutingModule, SharedModule, AuthModule
